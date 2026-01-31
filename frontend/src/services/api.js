956f93ec514a96cad554d3e5e867809ed609b93b
rahim2025 <mohammad.rahim.bhuiyan20@gmail.com>
2026-01-31 19:57:19 +0600

    test

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = "https://chottola-e-commerce-website-git-613bf5-mohammad-rahims-projects.vercel.app/api" || 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://chottola-e-commerce-website-git-613bf5-mohammad-rahims-projects.vercel.app/api",
   headers: {
     'Content-Type': 'application/json'
   }
